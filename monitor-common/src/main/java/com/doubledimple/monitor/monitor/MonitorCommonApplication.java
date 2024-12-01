376dd5a307283d10ab6fad68ba694ae6d9410368
doubleDimple <lovele.cn@gmail.com>
2024-12-01 10:43:11 +0800

    first commit

---
+++ monitor-common/src/main/java/com/doubledimple/monitor/monitor/MonitorCommonApplication.java
@@ -0,0 +1,13 @@
+package com.doubledimple.monitor.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorCommonApplication.class, args);
+    }
+
+}
